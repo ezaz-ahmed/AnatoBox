ed6fb87bd2c944d57255262f1901bea4333f7924
Ezaz Ahmed <ezazahmed110@gmail.com>
2025-04-16 22:21:18 +0600

    ✨ feat(ui) - add brutalism style and new components
    - Added new brutalism design style
    - Created Button component
    - Created Input component
    - Created Progress component
    - Created Textarea component
    - Created Checkbox component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,40 @@
 import { Button } from "~/components/ui/button";
+import { Checkbox } from "~/components/ui/checkbox";
+import { Input } from "~/components/ui/input";
+import { Progress } from "~/components/ui/progress";
+import { Textarea } from "~/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive">
-        Testing Schadcn
-      </Button>
+    <div className=" p-4">
+
+
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button>
+            Testing Schadcn
+          </Button>
+        </div>
+        <div>
+          <Input placeholder="input" />
+        </div>
+
+        <div>
+          <Textarea placeholder="input" />
+        </div>
+
+        <div>
+          <Textarea placeholder="input" />
+        </div>
+
+        <div>
+          <Progress value={50} />
+        </div>
+
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
